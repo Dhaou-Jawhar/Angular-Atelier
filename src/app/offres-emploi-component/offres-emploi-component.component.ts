fd0a48f6fdefbce86159617b444591be8c28f179
Jawhar <jawher.dhaou@esprit.tn>
2023-02-05 20:10:33 +0100

    la liste des emplois

--- src/app/offres-emploi-component/offres-emploi-component.component.ts
+++ src/app/offres-emploi-component/offres-emploi-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Emploi } from '../core/model/emploi';
 
 @Component({
   selector: 'app-offres-emploi-component',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offres-emploi-component.component.css']
 })
 export class OffresEmploiComponentComponent implements OnInit {
-
+  title:string = "Welcom";
+  listEmlpois!:Emploi[];
   constructor() { }
 
+
   ngOnInit(): void {
+    this.listEmlpois=[
+      {reference: "SE5", titre: "Full Stuck", entreprise: "Nasa", etat: true},
+      {reference: "G313", titre: "Front End", entreprise: "Google", etat: false},
+      {reference: "3B7", titre: "Back End", entreprise: "Amazon", etat: false}, ]
+    
   }
+
+  postuler(i:number){
+    this.listEmlpois[i].etat==false;
+}
 
 }
