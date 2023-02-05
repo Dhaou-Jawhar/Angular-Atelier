c31fcee3e19553d4e609ee1355c299d616fe3188
Jawhar <jawher.dhaou@esprit.tn>
2023-02-05 19:43:41 +0100

    creation composant

---
+++ src/app/offres-emploi-component/offres-emploi-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offres-emploi-component',
+  templateUrl: './offres-emploi-component.component.html',
+  styleUrls: ['./offres-emploi-component.component.css']
+})
+export class OffresEmploiComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
