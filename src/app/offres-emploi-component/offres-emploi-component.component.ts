385c3f4e32204efe921f33295c50f1943f8b0e46
Jawhar <jawher.dhaou@esprit.tn>
2023-02-08 00:02:04 +0100

    recherche

--- src/app/offres-emploi-component/offres-emploi-component.component.ts
+++ src/app/offres-emploi-component/offres-emploi-component.component.ts
@@ -8,15 +8,15 @@ import { Emploi } from '../core/model/emploi';
 })
 export class OffresEmploiComponentComponent implements OnInit {
   title:string = "Welcom";
-  nmbr:number = 5;
   listEmlpois!:Emploi[];
+  Entreprise = '';
   constructor() { }
 
 
   ngOnInit(): void {
     this.listEmlpois=[
       {reference: "SE5", titre: "Full Stuck", entreprise: "Nasa", etat: true},
-      {reference: "G313", titre: "Front End", entreprise: "Google", etat: false},
+      {reference: "G313", titre: "Front End", entreprise: "Nasa", etat: false},
       {reference: "3B7", titre: "Back End", entreprise: "Amazon", etat: false}, ]
     
   }
@@ -38,6 +38,23 @@ calculerB(){
     let bilan = this.listEmlpois.filter(e => !e.etat).length;
     alert(`Il y a ${bilan} offres d'emploi non clôturées.`);
   }
+
+  /* rechercher(value: string) {
+    this.Entreprise = value;
+  } */
+
+  recherche(value:string){
+    let resultats = [];
+    for (const emploi of this.listEmlpois){
+      if(value==emploi.entreprise){
+        resultats.push(emploi);
+      }
+    }
+    this.listEmlpois = resultats;
+    return resultats;
+  }
+
+  
 }
 
 
